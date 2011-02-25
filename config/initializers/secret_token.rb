fefdf34ec54fdd0bd3d96ed2d138771f3d4efef6
waiyee <wynntsui@gmail.com>
2011-02-25 14:28:35 +0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FypApp::Application.config.secret_token = '4931d2700713d4741f7d7074271f1c9310a4f1e1e1d5f7bfb9b142ddbb7886c39ce7071d0865acc7a18a548bd341bd8ec3b8e00aaa90f65fcc05b3651c6d074f'
